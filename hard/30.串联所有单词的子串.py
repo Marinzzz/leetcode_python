950ccc673455a8ebe4887605911cada88145d87e
Marinzzz <2318672386@qq.com>
2020-03-22 19:44:11 +0800

    3.21_30.串联所有单词的子串

---
+++ hard/30.串联所有单词的子串.py
@@ -0,0 +1,58 @@
+from typing import re
+
+
+class Solution_1:
+    def findSubstring(self, s: str, words):
+        record = []
+        ret = []
+
+        def GetAllSubstring(str_list, depth):
+            if depth == len(str_list) - 1:
+                record.append(''.join(str_list))
+            for i in range(depth, len(str_list)):
+                temp = [x for x in str_list]
+                change_one = temp[i]
+                temp[i] = temp[depth]
+                temp[depth] = change_one
+                GetAllSubstring(temp, depth + 1)
+        GetAllSubstring(words, 0)
+        if not record:
+            return []
+        for i in range(0, len(s) - len(record[0])):
+            if s[i:i + len(record[0])] in record:
+                ret.append(i)
+        return ret
+
+
+class Solution:
+    def findSubstring(self, s, words):
+        ret = []
+        if not words:
+            return ret
+        import collections
+        word_dic = collections.Counter(words)
+        len_word = len(words[0])
+        length = len(''.join(words))
+        for i in range(0, len(s) - length + 1):
+            temp = s[i: i + length]
+            import re
+            temp_arr = re.findall(r'.{%d}' % len_word, temp)
+            flag = 0
+            temp_dict = collections.Counter(temp_arr)
+            for key, value in temp_dict.items():
+                if value > word_dic[key]:
+                    flag = 1
+                    break
+            if not flag:
+                ret.append(i)
+        return ret
+
+
+
+if __name__ == "__main__":
+    a = Solution()
+    s = "pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel"
+    words = ["dhvf","sind","ffsl","yekr","zwzq","kpeo","cila","tfty","modg","ztjg","ybty","heqg","cpwo","gdcj","lnle","sefg","vimw","bxcb"]
+    print(a.findSubstring(s, words))
+
+
